7d97332dbbeb68dafafc2d3bf32dc655fcfe4e62
Gấu <115764988+bichngoc55@users.noreply.github.com>
2024-10-17 00:37:24 +0700

    set up

---
+++ src/main/java/com/jewlery/morri/config/firebaseConfig.java
@@ -0,0 +1,25 @@
+package com.jewlery.morri.config;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import java.io.FileInputStream;
+import com.google.firebase.FirebaseOptions;
+import java.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class firebaseConfig {
+    @Bean
+    public FirebaseApp firebaseService() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl(System.getenv("FIREBASE_DATABASE_URL"))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+
+    }
+    
+}
