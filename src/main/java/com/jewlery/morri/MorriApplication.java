1f24c0873e9d34ff6f8b545d3eb8d5dabaf61662
Gấu <115764988+bichngoc55@users.noreply.github.com>
2024-10-16 22:44:30 +0700

    first commit

---
+++ src/main/java/com/jewlery/morri/MorriApplication.java
@@ -0,0 +1,13 @@
+package com.jewlery.morri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorriApplication.class, args);
+	}
+
+}
